d502454527328173de8d6083029e667ede7dc9b7
bcaliva21 <bcaliva21@gmail.com>
2023-02-07 11:20:19 -0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/App.css'
+
+import Factory from './Factory'
+
+function App() {
+    return (
+        <div className="App">
+            <Factory />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Factory.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './css/Factory.css'
+
+const Factory = () => {
+    return <div>Factory</div>
+}
+
+export default Factory
