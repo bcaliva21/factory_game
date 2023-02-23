6d0a01a45503b1a18b50e56a73206e2e6820356b
bcaliva21 <bcaliva21@gmail.com>
2023-02-23 08:06:22 -0600

    feat(items.tsx): implement animation; enhance component to accept an animation prop;

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,14 +1,23 @@
 import react from 'react'
-import styled from 'styled-components'
+import styled, { Keyframes, keyframes } from 'styled-components'
 
-const RawItem = styled.div<{ color: string }>`
+const RawItem = styled.div<{ color: string; animation: Keyframes }>`
+    position: absolute;
     width: 50px;
     height: 50px;
     background-color: ${({ color }) => color};
+    animation: ${({ animation }) => animation && animation} ease-out;
+    animation-duration: 6s;
 `
 
-const Item = ({ color }: { color: string }) => {
-    return <RawItem color={color} />
+const Item = ({
+    color,
+    animation,
+}: {
+    color: string
+    animation: Keyframes
+}) => {
+    return <RawItem color={color} animation={animation} />
 }
 
 export default Item
