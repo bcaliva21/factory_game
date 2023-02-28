162edeb16c514a5fb89ce44c8e5bfab3e5e1bfaa
bcaliva21 <bcaliva21@gmail.com>
2023-02-28 14:56:26 -0600

    feat(conveyorBelt): add harddrive

--- src/components/inside/ConveyorBelt.tsx
+++ src/components/inside/ConveyorBelt.tsx
@@ -19,6 +19,77 @@ const LeftLeg = styled.div`
     z-index: 110;
 `
 
+const HDContainer = styled.div`
+    position: relative;
+    margin-top: 25%;
+    background-color: transparent;
+    width: 100%;
+    height: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Case = styled.div`
+    position: relative;
+    width: 50%;
+    height: 50%;
+    background-color: transparent;
+    border: 3px solid black;
+    border-radius: 10px;
+`
+
+const CaseLine = styled.div`
+    width: 99%;
+    position: absolute;
+    bottom: 25%;
+    border: 2px solid black;
+`
+
+const blinkGreen = keyframes`
+    from {
+        background-color: green;
+    }
+    to {
+        background-color: transparent;
+    }
+`
+    
+const BlinkingLightGreen = styled.div`
+    width: 5%;
+    height: 5%;
+    background-color: transparent;
+    position: absolute;
+    bottom: 10%;
+    right: 20%;
+    border: 1px solid green;
+    border-radius: 5%;
+    animation: ${blinkGreen} infinite;
+    animation-duration: 1s;
+`
+
+const blinkYellow = keyframes`
+    from {
+        background-color: yellow;
+    }
+    to {
+        background-color: transparent;
+    }
+`
+    
+const BlinkingLightYellow = styled.div`
+    width: 5%;
+    height: 5%;
+    background-color: transparent;
+    position: absolute;
+    bottom: 10%;
+    right: 30%;
+    border: 1px solid yellow;
+    border-radius: 5%;
+    animation: ${blinkYellow} infinite;
+    animation-duration: 1.50s;
+`
+
 const RightLeg = styled.div`
     position: absolute;
     background-color: grey;
@@ -65,7 +136,15 @@ const BottomBelt = styled.div`
 const ConveyorBelt = () => {
     return (
         <ConveyorBeltContainer>
-            <LeftLeg />
+            <LeftLeg>
+                <HDContainer>
+                    <Case>
+                        <CaseLine />
+                        <BlinkingLightGreen />
+                        <BlinkingLightYellow />
+                    </Case>
+                </HDContainer>
+            </LeftLeg>
             <RightLeg />
             <TopBelt />
             <BottomBelt />
