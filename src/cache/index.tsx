ee4a0d7cfb868b1357baa27fd32f02e99da60c3f
Bradley Caliva <bcaliva21@gmail.com>
2023-03-10 15:29:53 -0600

    feat(cache): remove gameInProgress references

--- src/cache/index.tsx
+++ src/cache/index.tsx
@@ -15,23 +15,16 @@ export const cache: InMemoryCache = new InMemoryCache({
 						return gameStateVar()
 					}
 				},
-                gameInProgress: {
-                    read() {
-                        return gameInProgressVar()
-                    }
-                }
-            }
+           }
         }
     }
 })
 
 // init values
 const isInsideVarIntialValue = false
-const gameInProgressVarInitialValue = false
 const gameStateVarInitialValue = GAME_STATE_TYPES.NOT_STARTED
 
 // helpers
 export const isInsideVar: ReactiveVar<boolean> = makeVar<boolean>(isInsideVarIntialValue)
-export const gameInProgressVar: ReactiveVar<boolean> = makeVar<boolean>(gameInProgressVarInitialValue)
 export const gameStateVar: ReactiveVar<GAME_STATE_TYPES> = makeVar<GAME_STATE_TYPES>(gameStateVarInitialValue)
 
