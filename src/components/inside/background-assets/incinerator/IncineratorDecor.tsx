faa0cb9e27d4a65b0c933875bb2842ba5c5fd48f
Bradley Caliva <bcaliva21@gmail.com>
2023-03-11 09:58:56 -0600

    refactor(highscoreplacard): design changes

--- src/components/inside/background-assets/incinerator/IncineratorDecor.tsx
+++ src/components/inside/background-assets/incinerator/IncineratorDecor.tsx
@@ -4,28 +4,42 @@ import styled from 'styled-components'
 const DecorContainer = styled.div`
 	width: 100%;
 	height: 30%;
-	background-color: green;
+	background-color: #901000;
 	position: absolute;
 	bottom: 30%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `
 
 const HighScoreContainer = styled.div`
-	width: 25%;
-	height: 20%;
+	width: 40%;
+	height: 40%;
+    background: linear-gradient(-45deg, rgba(100,100,100,0.22), rgba(200,200,200,0.25));
+    box-shadow: 6px 6px 8px 0 rgba(0, 0, 0, 0.25),
+    -4px -4px 6px 0 rgba(200, 200, 200, 0.3);
+	background: linear-gradient(-45deg, rgba(0,0,0,0.22), rgba(200,200,200,0.25));
+	border-radius: 10px;
 `
 
 const NamePlate = styled.div`
 	width: 100%;
 	height: 50%;
 	font-size: 14px;
-	color: black;
+	color: #420700;
+	text-transform: uppercase;
+	text-align: center;
+	text-justify: center;
 `
 
 const ScorePlate = styled.div`
 	width: 100%;
 	height: 50%;
 	font-size: 14px;
-	color: black;
+	color: #420700;
+	text-transform: uppercase;
+	text-align: center;
+	text-justify: center;
 `
 
 const IncineratorDecor = () => {
