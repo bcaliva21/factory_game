1ad909519311a967ad5a5dc27e06828f387b4494
Bradley Caliva <bcaliva21@gmail.com>
2023-03-11 13:52:53 -0600

    fix(incinerator-door): sizing fix

--- src/components/inside/background-assets/incinerator/IncineratorDoor.tsx
+++ src/components/inside/background-assets/incinerator/IncineratorDoor.tsx
@@ -6,15 +6,17 @@ const IncineratorDoorContainer = styled.div`
     height: 30%;
     position: relative;
     display: flex;
-    flex-direction: row-reverse;
+	align-items: end;
+	flex-direction: row-reverse;
     border-top: 10px solid #282828;
     border-right: 5px solid #282828;
     border-left: 2px solid #282828;
+	background-color: #282828;
 `
 
 const STRONGSIDE = styled.div`
     background-color: #282828;
-    height: 100%;
+    height: 80%; 
     width: 80%;
     z-index: 20;
 `
@@ -22,7 +24,7 @@ const STRONGSIDE = styled.div`
 const WEAKSIDE = styled.div`
     background: rgb(255,255,255);
     background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(232,232,232,1) 47%, rgba(196,192,192,1) 100%);
-    height: 100%;
+    height: 80%;
     width: 20%;
     z-index: 0;
 `
