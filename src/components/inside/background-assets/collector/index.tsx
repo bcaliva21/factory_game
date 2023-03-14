6ed7e6ab1de5adc9fbd0b74c534cba555539af10
Bradley Caliva <bcaliva21@gmail.com>
2023-03-14 17:38:31 -0500

    refactor(colllector): remove door

--- src/components/inside/background-assets/collector/index.tsx
+++ src/components/inside/background-assets/collector/index.tsx
@@ -52,17 +52,6 @@ const ItemSectionGlass = styled.div`
     z-index: 50;
 `
 
-const CollectorDoorContainer = styled.div`
-    width: 60%;
-    height: 65%;
-    position: relative;
-    display: flex;
-    flex-direction: row-reverse;
-    border-top: 10px solid #282828;
-    border-right: 5px solid #282828;
-    border-left: 5px solid #282828;
-`
-
 const ItemCollectorRow = styled.div`
     display: flex;
     flex-direction: row;
@@ -70,26 +59,6 @@ const ItemCollectorRow = styled.div`
     widht: 100%;
     align-items: center;
     justify-content: center;
-`
-
-const STRONGSIDE = styled.div`
-    background-color: #282828;
-    height: 100%;
-    width: 85%;
-    z-index: 20;
-`
-
-const WEAKSIDE = styled.div`
-    background: rgb(255, 255, 255);
-    background: linear-gradient(
-        90deg,
-        rgba(255, 255, 255, 1) 0%,
-        rgba(232, 232, 232, 1) 47%,
-        rgba(196, 192, 192, 1) 100%
-    );
-    height: 100%;
-    width: 15%;
-    z-index: 0;
 `
 
 const BuildingStripe = styled.div`
@@ -143,11 +112,7 @@ const Collector = () => {
     return (
         <CollectorContainer>
             <CollectorBottomSection>
-                <CollectorDoorContainer>
-                    <STRONGSIDE />
-                    <WEAKSIDE />
-                </CollectorDoorContainer>
-            </CollectorBottomSection>
+           </CollectorBottomSection>
             <BuildingStripe />
             <BuildingStripeTwo />
             <ItemSectionGlass />
