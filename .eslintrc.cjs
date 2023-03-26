42faaa0c1b0879a835c9bddca80aaa0bd8ba2672
bcaliva21 <bcaliva21@gmail.com>
2023-03-25 21:14:24 -0700

    feat(frontend): move src files to frontend dir

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,10 +17,9 @@ module.exports = {
     },
     "rules": {
         "@typescript-eslint/no-unused-vars": "error",
-        // to enforce using type for object type definitions, can be type or interface 
+        // to enforce using type for object type definitions, can be type or interface
         "@typescript-eslint/consistent-type-definitions": ["error", "type"],
         "@typescript-eslint/ban-ts-comment": "off",
     },
     "plugins": ["@typescript-eslint"],
-    "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 }
